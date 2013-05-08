ee5d942027c188d8f68064a63082e928a6877b99
Jeff <Jeff@Hanspoop>
2013-05-07 22:52:29 -0400

    Template Build

--- Clossette/gen/com/closette/R.java
+++ Clossette/gen/com/closette/R.java
@@ -21,12 +21,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int cameraicon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int jeans2=0x7f020002;
+        public static final int skirt1=0x7f020003;
+        public static final int skirt2=0x7f020004;
+        public static final int top1=0x7f020005;
+        public static final int top2=0x7f020006;
     }
     public static final class id {
-        public static final int BottomGallery=0x7f080001;
-        public static final int TopGallery=0x7f080000;
-        public static final int action_settings=0x7f080002;
+        public static final int BottomGallery=0x7f080003;
+        public static final int TopGallery=0x7f080001;
+        public static final int action_settings=0x7f080004;
+        public static final int textViewBottom=0x7f080002;
+        public static final int textViewTop=0x7f080000;
     }
     public static final class layout {
         public static final int activity_closette_main=0x7f030000;
--- Clossette/src/com/closette/ClosetteMain.java
+++ Clossette/src/com/closette/ClosetteMain.java
@@ -1,16 +1,21 @@
 package com.closette;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 
 public class ClosetteMain extends Activity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_closette_main);
-    }
 
 
     @Override
@@ -20,4 +25,105 @@ public class ClosetteMain extends Activity {
         return true;
     }
     
+
+    private ImageView selectedImageView;
+
+    private ImageView leftArrowImageView;
+
+    private ImageView rightArrowImageView;
+
+    private Gallery topgallery;
+    private Gallery bottomgallery;
+
+    private int selectedImagePosition = 0;
+
+    private List<Drawable> drawables;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_closette_main);
+
+        getDrawablesList();
+        setupUI();
+    }
+
+    private void setupUI() {
+
+        topgallery = (Gallery) findViewById(R.id.TopGallery);
+        bottomgallery = (Gallery) findViewById(R.id.BottomGallery);
+
+  
+
+        topgallery.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+                selectedImagePosition = pos;
+
+ 
+
+                changeBorderForSelectedImage(selectedImagePosition);
+                setSelectedImage(selectedImagePosition);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+
+        });
+
+//        galImageAdapter = new GalleryImageAdapter(this, drawables);
+//
+//        topgallery.setAdapter(galImageAdapter);
+
+        if (drawables.size() > 0) {
+
+            topgallery.setSelection(selectedImagePosition, false);
+
+        }
+
+//        if (drawables.size() == 1) {
+//
+//            rightArrowImageView.setImageDrawable(getResources().getDrawable(R.drawable.arrow_right_disabled));
+//        }
+
+    }
+
+    private void changeBorderForSelectedImage(int selectedItemPos) {
+
+//        int count = topgallery.getChildCount();
+//
+//        for (int i = 0; i < count; i++) {
+//
+//            ImageView imageView = (ImageView) topgallery.getChildAt(i);
+//            imageView.setBackgroundDrawable(getResources().getDrawable(R.drawable.image_border));
+//            imageView.setPadding(3, 3, 3, 3);
+//
+//        }
+//
+//        ImageView imageView = (ImageView) gallery.getSelectedView();
+//        imageView.setBackgroundDrawable(getResources().getDrawable(R.drawable.selected_image_border));
+//        imageView.setPadding(3, 3, 3, 3);
+    
+    }
+    
+
+    private void getDrawablesList() {
+
+        drawables = new ArrayList<Drawable>();
+        drawables.add(getResources().getDrawable(R.drawable.skirt1));
+        drawables.add(getResources().getDrawable(R.drawable.skirt2));
+
+    }
+
+    private void setSelectedImage(int selectedImagePosition) {
+
+        BitmapDrawable bd = (BitmapDrawable) drawables.get(selectedImagePosition);
+        Bitmap b = Bitmap.createScaledBitmap(bd.getBitmap(), (int) (bd.getIntrinsicHeight() * 0.9), (int) (bd.getIntrinsicWidth() * 0.7), false);
+        selectedImageView.setImageBitmap(b);
+        selectedImageView.setScaleType(ScaleType.FIT_XY);
+
+    }
 }
